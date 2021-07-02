0ee7fed589d234552036b31624cbae11fa7827d3
jebitok-dev <sharonjebitok@gmail.com>
2021-07-02 18:27:20 +0300

    Add Changes to Recipe

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Recipes = () => (
-  <div>
-    <h2>Recipes</h2>
-  </div>
-);
+const Recipes = (recipe, meal, letter, media, id) => {
+  let data = '';
+  if (media.length) {
+    data = media[0].small;
+  }
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      <div className="image-container">
+        <img src={data} alt={meal} />
+      </div>
+      <div className="info">
+        <h1>{meal}</h1>
+        <h2>{`${recipe} - ${letter}` }</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Recipes;
