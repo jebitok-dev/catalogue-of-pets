88686239f8070c9269c3c860319510e2e79c788a
jebitok-dev <sharonjebitok@gmail.com>
2021-07-04 17:55:04 +0300

    Add Changes to Search Params

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,58 +1,61 @@
-import React, {useState, useEffect} from 'react';
-import pet, {ANIMALS} from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import Dropdown from '../containers/Dropdown';
 
 const SearchParams = () => {
-    const [location, updateLocation] = useState('Seattle, WA');
-    const [breeds, updateBreeds] = useState([]);
-    const [animal, AnimalDropDown] = Dropdown('Breed', '', breeds);
-    const [Pets, setPets] = useState([]);
+  const [location, updateLocation] = useState('Seattle, WA');
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropDown] = Dropdown('Breed', '', breeds);
+  const [Pets, setPets] = useState([]);
 
-    async function requestPets() {
-        const {animals} = await pet.animals({
-            location,
-            breed,
-            type: animal,
-        });
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
 
-        setPets(animals || []);
-    }
+    setPets(animals || []);
+  }
 
-    useEffect(() => {
-        updateBreeds([]);
-        updateBreed('');
-        pet.breeds(animal).then(({breeds}) => {
-            const breedItems= breeds.map(({name}) => name);
-            updateBreeds(breedItems);
-        });
-        [animal, updateBreed, updateBreeds])
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed('');
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedItems = breeds.map(({ name }) => name);
+      updateBreeds(breedItems);
+    });
+  }, [animal, updateBreed, updateBreeds]);
 
-        return (
-            <div className="search-params">
-                {name}
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    requestPets();
-                }}
-                >
-                    <label 
-                        htmlFor="location">
-                            location
-                            <input
-                                id="location"
-                                value={location}
-                                onChange={(e) => updateLocation(e.target.value)}
-                            />
-                        </label>
-                        <AnimalDropDown />
-                        <BreedDropDown />
-                        <button
-                            type="submit"
-                        >Submit</button>
-                </form>
-            </div>
-        )
-    })
-}
+  return (
+    <div className="search-params">
+      {name}
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        requestPets();
+      }}
+      >
+        <label
+          htmlFor="location"
+        >
+          location
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropDown />
+        <BreedDropDown />
+        <button
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      <Results pets={Pets} />
+    </div>
+  );
+};
 
 export default SearchParams;
