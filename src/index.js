96b355d1e952fb15538da0e133b30874624bebc6
jebitok-dev <sharonjebitok@gmail.com>
2021-07-10 22:47:25 +0300

    Add Changes to Index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, createStore } from 'react-redux';
+
+import { Router } from './Routes/Route';
 import './index.css';
+import reducer from './Reducers';
 import App from './Components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
