3195cb86757737825ff93cdad05d400a4444c603
jebitok-dev <sharonjebitok@gmail.com>
2021-07-12 04:45:30 +0300

    Add Filter Reducer Tests

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import filterPets from './Filter';
+import filter from './Filter';
 import pets from './Pet';
 
 export default combineReducers({
   pets,
-  filterPets,
+  filter,
 });
