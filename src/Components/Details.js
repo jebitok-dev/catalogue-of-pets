8f46cb995cb83be95bc0f1e3e05ed8f7b0278b07
jebitok-dev <sharonjebitok@gmail.com>
2021-07-23 10:50:44 +0300

    Fix Broken Images

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -46,7 +46,7 @@ const Details = ({ id }) => {
   return (
     <div className="details">
       <div className="image-container">
-        <img onError={avatar} src={avatar} alt="" />
+        <img onError="this.style.display='none'" src={avatar} alt="" />
       </div>
       <div className="details-section">
         <h1>{name}</h1>
